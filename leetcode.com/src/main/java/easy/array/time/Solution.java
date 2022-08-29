549897f7ff2df3f45e6d655b1c8011fac93ec84f
Dmitrii Sobin <neo7976@yandex.ru>
2022-08-29 15:24:08 +0300

    Попытка написания кода

--- leetcode.com/src/main/java/easy/array/time/Solution.java
+++ leetcode.com/src/main/java/easy/array/time/Solution.java
@@ -3,6 +3,19 @@ package easy.array.time;
 public class Solution {
 
     public int maxProfit(int[] prices) {
-        return 0;
+        int profit = 0, max = 0, min = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            min = prices[i];
+            if (prices[i] > prices[i + 1] && min == 0) {
+                min = prices[i + 1];
+            } else if (min != 0 && prices[i] < prices[i + 1]) {
+                max = prices[i + 1] - min;
+                min = 0;
+                profit += max;
+            } else if (min < prices[i]){
+
+            }
+        }
+        return profit;
     }
 }
