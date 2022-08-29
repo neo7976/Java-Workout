7ba18fa505cdf594d7fc5a92f16f52b490ee98fc
Dmitrii Sobin <neo7976@yandex.ru>
2022-08-29 13:19:08 +0300

    Метод быстрой сортировки

--- leetcode.com/src/main/java/easy/array/remove/Remove.java
+++ leetcode.com/src/main/java/easy/array/remove/Remove.java
@@ -1,5 +1,7 @@
 package easy.array.remove;
 
+import java.util.Arrays;
+
 public class Remove {
 
     private String[] arrayInput = {"0", "0", "1", "1", "1", "2", "2", "3", "3", "4"};
@@ -42,24 +44,55 @@ public class Remove {
     public String[] getArrayInput() {
         return arrayInput;
     }
-    //    public void pivoting(int[] array, int pi) {
-//        int left = 0;
-//        int right = 0;
-//        int pivot = array[pi];
-//        while (true) {
-//            while (array[left] < pivot) {
-//                left += 1;
-//            }
-//            while (array[right] < pivot) {
-//                right -= 1;
-//            }
-//            if (left >= right)
-//                pivot = left;
-//            int x = array[left];
-//            array[left] = array[right];
-//            array[right] = x;
-//        }
-//    }
 
+    public static void quickSort(int[] array, int low, int high) {
+        if (array.length == 0)
+            return;//завершить выполнение, если длина массива равна 0
 
+        if (low >= high)
+            return;//завершить выполнение если уже нечего делить
+
+        // выбрать опорный элемент
+        int middle = low + (high - low) / 2;
+        int opora = array[middle];
+
+        // разделить на подмассивы, который больше и меньше опорного элемента
+        int i = low, j = high;
+        while (i <= j) {
+            while (array[i] < opora) {
+                i++;
+            }
+
+            while (array[j] > opora) {
+                j--;
+            }
+
+            if (i <= j) {//меняем местами
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        // вызов рекурсии для сортировки левой и правой части
+        if (low < j)
+            quickSort(array, low, j);
+
+        if (high > i)
+            quickSort(array, i, high);
+    }
+    public static void main(String[] args) {
+        int[] x = { 8, 0, 4, 7, 3, 7, 10, 12, -3 };
+        System.out.println("Было");
+        System.out.println(Arrays.toString(x));
+
+        int low = 0;
+        int high = x.length - 1;
+
+        quickSort(x, low, high);
+        System.out.println("Стало");
+        System.out.println(Arrays.toString(x));
+    }
 }
