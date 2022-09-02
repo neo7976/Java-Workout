b8f917ef79c2cbce7a68112c2a430541cc0e5303
Dmitrii Sobin <neo7976@yandex.ru>
2022-09-02 15:53:34 +0300

    Рефакторинг + неплохие решения

--- src/main/java/codewars/fivekyu/Greed.java
+++ src/main/java/codewars/fivekyu/Greed.java
@@ -11,6 +11,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Greed {
@@ -32,12 +33,14 @@ public class Greed {
             put(List.of(1), 100);
             put(List.of(5), 50);
         }};
+
         Map<List<Integer>, Integer> sortedMap = map.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
                         Map.Entry::getValue,
                         (e1, e2) -> e1,
                         LinkedHashMap::new));
+
         List<Integer> listDel = Arrays.stream(dice).boxed().sorted().toList();
         List<Integer> list = new ArrayList<>(listDel);
         System.out.println(list);
@@ -46,7 +49,6 @@ public class Greed {
             for (int i = 0; i < 3; i++) {
                 if (list.size() >= 3) {
                     List<Integer> listToCopy = list.subList(i, i + 3);
-
                     if (listToCopy.equals(listIntegerEntry.getKey())) {
                         System.out.println("Ключ " + listIntegerEntry.getKey() + ": Cписок:" + listToCopy + "-" + listToCopy.equals(listIntegerEntry.getKey()));
                         list.subList(i, i + 3).clear();
@@ -55,7 +57,6 @@ public class Greed {
                     }
                 }
             }
-
             for (int j = 0; j < list.size(); j++) {
                 List<Integer> listToCopy = list.subList(j, j + 1);
                 if (listToCopy.equals(listIntegerEntry.getKey())) {
@@ -74,6 +75,37 @@ public class Greed {
         int n[] = new int[7];
         for (int d : dice) n[d]++;
         return n[1] / 3 * 1000 + n[1] % 3 * 100 + n[2] / 3 * 200 + n[3] / 3 * 300 + n[4] / 3 * 400 + n[5] / 3 * 500 + n[5] % 3 * 50 + n[6] / 3 * 600;
+    }
+
+    public int greedy2(int[] dice) {
+        Map<Integer, Long> collect = Arrays.stream(dice).boxed().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        for (Entry<Integer, Long> integerLongEntry : collect.entrySet()) {
+            System.out.println("Ключ: " + integerLongEntry.getKey() + " Значение: " + integerLongEntry.getValue());
+        }
+        int score = 0;
+        for (Map.Entry<Integer, Long> e : collect.entrySet()) {
+            switch (e.getKey()) {
+                case 1:
+                    score += ((e.getValue() >= 3) ? 1000 : 0) + (e.getValue() % 3) * 100;
+                    break;
+                case 2:
+                    score += ((e.getValue() >= 3) ? 200 : 0);
+                    break;
+                case 3:
+                    score += ((e.getValue() >= 3) ? 300 : 0);
+                    break;
+                case 4:
+                    score += ((e.getValue() >= 3) ? 400 : 0);
+                    break;
+                case 5:
+                    score += ((e.getValue() >= 3) ? 500 : 0) + (e.getValue() % 3) * 50;
+                    break;
+                case 6:
+                    score += ((e.getValue() >= 3) ? 600 : 0);
+                    break;
+            }
+        }
+        return score;
     }
 }
 
