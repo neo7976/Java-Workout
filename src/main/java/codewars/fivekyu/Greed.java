1c3deb9a8804f64c23ef02c082867e3b3648b3d2
Dmitrii Sobin <neo7976@yandex.ru>
2022-09-05 11:05:27 +0300

    SumDigPower.java

--- src/main/java/codewars/fivekyu/Greed.java
+++ src/main/java/codewars/fivekyu/Greed.java
@@ -85,24 +85,12 @@ public class Greed {
         int score = 0;
         for (Map.Entry<Integer, Long> e : collect.entrySet()) {
             switch (e.getKey()) {
-                case 1:
-                    score += ((e.getValue() >= 3) ? 1000 : 0) + (e.getValue() % 3) * 100;
-                    break;
-                case 2:
-                    score += ((e.getValue() >= 3) ? 200 : 0);
-                    break;
-                case 3:
-                    score += ((e.getValue() >= 3) ? 300 : 0);
-                    break;
-                case 4:
-                    score += ((e.getValue() >= 3) ? 400 : 0);
-                    break;
-                case 5:
-                    score += ((e.getValue() >= 3) ? 500 : 0) + (e.getValue() % 3) * 50;
-                    break;
-                case 6:
-                    score += ((e.getValue() >= 3) ? 600 : 0);
-                    break;
+                case 1 -> score += ((e.getValue() >= 3) ? 1000 : 0) + (e.getValue() % 3) * 100;
+                case 2 -> score += ((e.getValue() >= 3) ? 200 : 0);
+                case 3 -> score += ((e.getValue() >= 3) ? 300 : 0);
+                case 4 -> score += ((e.getValue() >= 3) ? 400 : 0);
+                case 5 -> score += ((e.getValue() >= 3) ? 500 : 0) + (e.getValue() % 3) * 50;
+                case 6 -> score += ((e.getValue() >= 3) ? 600 : 0);
             }
         }
         return score;
