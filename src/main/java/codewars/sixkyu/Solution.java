8b6a3982a0e214525df686230ddf137ce7a1ffaf
Dmitrii Sobin <neo7976@yandex.ru>
2022-09-05 13:42:11 +0300

    Solution.java - Когда-нибудь доделать

--- src/main/java/codewars/sixkyu/Solution.java
+++ src/main/java/codewars/sixkyu/Solution.java
@@ -1,7 +1,43 @@
 package codewars.sixkyu;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+//todo Ещё не проходил многопоточность, поэтому код не работает
 public class Solution {
-    public  int solveSuperMarketQueue(int[] customers, int n) {
-        return 0;
+    public int solveSuperMarketQueue(int[] customers, int n) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int customer : customers) {
+            queue.add(customer);
+        }
+        int[] arrayValue = new int[n];
+        int count = 0;
+        AtomicInteger count1 = new AtomicInteger();
+
+        while (!queue.isEmpty()) {
+            if (arrayValue[0] == 0)
+                arrayValue[0] = queue.poll();
+            arrayValue[0]--;
+            count++;
+        }
+
+        if (n == 2) {
+            new Thread(() -> {
+
+                while (!queue.isEmpty())
+                    if (arrayValue[1] == 0)
+                        arrayValue[1] = queue.poll();
+                try {
+                    System.out.println("Привет");
+                    arrayValue[1]--;
+                    count1.getAndIncrement();
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }).start();
+        }
+        return Math.max(count, count1.intValue());
+
     }
 }
