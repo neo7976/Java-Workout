3e3e7c287bea239378d7c7570ee7c1ef9c8bc121
Dmitrii Sobin <neo7976@yandex.ru>
2022-09-05 11:35:53 +0300

    KataTwo.java

--- src/main/java/codewars/sixkyu/KataTwo.java
+++ src/main/java/codewars/sixkyu/KataTwo.java
@@ -1,8 +1,22 @@
 package codewars.sixkyu;
 
+import java.util.Arrays;
+
 public class KataTwo {
     public int findEvenIndex(int[] arr) {
-        // your code
-        return 0;
+        int memoryLeft = 0;
+        int memoryRight = 0;
+        for (int i : arr) {
+            memoryRight += i;
+        }
+//        memoryRight = Arrays.stream(arr).sum(); - просуммировать массив
+        for (int i = 0; i < arr.length; i++) {
+            memoryRight -= arr[i];
+            if (memoryLeft == memoryRight)
+                return i;
+            memoryLeft += arr[i];
+        }
+        return -1;
     }
+
 }
