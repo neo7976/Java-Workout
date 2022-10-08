d2055a7428b68cef113908df48563471a3789ece
Dmitrii Sobin <neo7976@yandex.ru>
2022-10-08 10:55:11 +0300

    Задача на многопоточность решена!

--- src/test/java/codewars/sixkyu/SolutionTest.java
+++ src/test/java/codewars/sixkyu/SolutionTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Stream;
 
 class SolutionTest {
@@ -27,6 +28,9 @@ class SolutionTest {
     public static Stream<Arguments> source() {
         return Stream.of(
                 Arguments.of(9, new int[]{2, 2, 3, 3, 4, 4}, 2),
+                Arguments.of(12, new int[]{5,3,4}, 1),
+                Arguments.of(10, new int[]{10,2,3,3}, 2),
+                Arguments.of(12, new int[]{2,3,10}, 2),
                 Arguments.of(0, new int[]{}, 1),
                 Arguments.of(15, new int[]{1, 2, 3, 4, 5}, 1)
         );
@@ -34,7 +38,7 @@ class SolutionTest {
 
     @ParameterizedTest
     @MethodSource("source")
-    void solveSuperMarketQueue(int expected, int[] array, int n) {
+    void solveSuperMarketQueue(int expected, int[] array, int n) throws ExecutionException, InterruptedException {
         Assertions.assertEquals(expected, solution.solveSuperMarketQueue(array, n), "Тест не прошел");
     }
 }
