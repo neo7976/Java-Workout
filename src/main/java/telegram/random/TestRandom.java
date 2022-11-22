48a02cf61b2858446f9a06b0e637d11c62db9010
Dmitrii Sobin <neo7976@yandex.ru>
2022-11-22 14:25:21 +0300

    Добавили числа в список

--- src/main/java/telegram/random/TestRandom.java
+++ src/main/java/telegram/random/TestRandom.java
@@ -5,6 +5,9 @@ import java.util.*;
 public class TestRandom {
     public static void main(String[] args) {
         //Создаем 25 чисел от 1 до 24
+        List<Integer> list = new ArrayList<>();
         (new Random()).ints(25, 1,25).sorted().forEach(System.out::println);
+        (new Random()).ints(25, 1,25).sorted().forEach(list::add);
+        System.out.println(list);
     }
 }
